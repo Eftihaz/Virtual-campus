8438cfaeb7b20e2fa9974f6b3295a345884e8b3a
Jamshed Hridoy <154745319+JAMSHED6@users.noreply.github.com>
2026-01-07 23:52:47 +0600

    menuitem.js

--- backend/model/MenuItem.js
+++ backend/model/MenuItem.js
@@ -4,8 +4,11 @@ const menuItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   available: { type: Boolean, default: true },
+  image: { type: String, default: '' },
   allergies: { type: [String], default: [] },
+  salesCount: { type: Number, default: 0 }, // Track number of times ordered
 }, { timestamps: true });
 
 module.exports = mongoose.models.MenuItem || mongoose.model('MenuItem', menuItemSchema);
+
 
